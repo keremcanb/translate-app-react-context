ed021d69791f55a81f578787528ee88d6723e229
keremcanb <keremcan@gmail.com>
2020-09-09 01:08:53 +0300

    eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
-class App extends React.Component {
-  state = { language: 'english' };
+const App = () => {
+  const [language, setLanguage] = useState('english');
 
-  onlanguagechange = (language) => {
-    this.setState({ language });
+  const onlanguagechange = (language) => {
+    setLanguage(language);
   };
 
-  render() {
-    return (
-      <div className='ui container'>
-        <div>
-          Select a language:{' '}
-          <i
-            className='flag us'
-            onClick={() => this.onlanguagechange('english')}
-          />
-          <i
-            className='flag nl'
-            onClick={() => this.onlanguagechange('dutch')}
-          />
-        </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <ColorContext.Provider value='primary'>
-            <UserCreate />
-          </ColorContext.Provider>
-        </LanguageContext.Provider>
+  return (
+    <div className='ui container'>
+      <div>
+        Select a language:{' '}
+        <i className='flag us' onClick={() => onlanguagechange('english')} />
+        <i className='flag nl' onClick={() => onlanguagechange('dutch')} />
       </div>
-    );
-  }
-}
+      <LanguageContext.Provider value={language}>
+        <ColorContext.Provider value='primary'>
+          <UserCreate />
+        </ColorContext.Provider>
+      </LanguageContext.Provider>
+    </div>
+  );
+};
 
 export default App;
